560ffdced3897fdc2f53a6a687157f39dbbc4eee
Tyler <tstep95@gmail.com>
2026-03-11 07:25:59 -0500

    more-detailed stats

--- eda/test_queries.py
+++ eda/test_queries.py
@@ -93,14 +93,41 @@ project_location = 'C:/Users/Tyler/Documents/GitHub/soccer-analytics-capstone-te
 #                      """)
 # print(y_coords)
 
-# y_coords = duckdb.sql(f"""
-                      
-#                       SELECT match_id, period, possession, possession_team_id, team_id, OFF_TEAM_COMPOSITION_PK, DEF_TEAM_COMPOSITION_PK, player_advantage, goal_diff,
-#                       SUM(duration) total_carry_duration, MAX(duration) max_carry_duration, COUNT(id) carries, COUNT(distinct player_id) players_carries
-#                       FROM read_parquet('{project_location}/eda/carry_level_stats.parquet')
-#                       GROUP BY match_id, period, possession, possession_team_id, team_id, OFF_TEAM_COMPOSITION_PK, DEF_TEAM_COMPOSITION_PK, player_advantage, goal_diff,
-#                      """)
-# print(y_coords)
+y_coords = duckdb.sql(f"""
+                      SELECT MIDFIELDERS_ATTACKING_GROUP_ID, FORWARDS_ATTACKING_GROUP_ID, team_id,
+                      --player_id, 
+                      EVENT_ZONE_END ,
+                      --MAX(completed_pass_length) max_completed_pass_length, AVG(completed_pass_length) avg_completed_pass_length,
+                      SUM(pass_goal_assist) pass_goal_assist, SUM(pass_shot_assist) pass_shot_assist, COUNT(completed_pass_length) completed_passes, COUNT(*) pass_attempts, 
+                      --BACKS_DEFENDING_GROUP_ID, MIDFIELDERS_DEFENDING_GROUP_ID, 
+                      --FORWARDS_ATTACKING_GROUP_ID, MIDFIELDERS_ATTACKING_GROUP_ID, 
+                      MIDFIELDERS_DEFENDING,  BACKS_DEFENDING, MIDFIELDERS_ATTACKING, FORWARDS_ATTACKING,
+                      FROM (
+                          SELECT 
+                          pl.team_id, player_id, EVENT_ZONE_END, --pass_length, 
+                          CASE WHEN pass_outcome IS NULL THEN pass_length ELSE NULL END AS completed_pass_length,
+                          --CASE WHEN EVENT_ZONE_END = EVENT_ZONE_START THEN 'Within' ELSE 'Outside' END AS PASS_WITHIN_ZONE,
+                      dtc.BACKS_GROUPING_ID BACKS_DEFENDING_GROUP_ID, dtc.MIDFIELDERS_GROUPING_ID MIDFIELDERS_DEFENDING_GROUP_ID,
+                      otc.FORWARDS_GROUPING_ID FORWARDS_ATTACKING_GROUP_ID, otc.MIDFIELDERS_GROUPING_ID MIDFIELDERS_ATTACKING_GROUP_ID,
+                          pass_goal_assist, pass_shot_assist, dtc.MIDFIELDERS MIDFIELDERS_DEFENDING, dtc.BACKS BACKS_DEFENDING, otc.MIDFIELDERS MIDFIELDERS_ATTACKING, otc.FORWARDS FORWARDS_ATTACKING
+                          FROM read_parquet('{project_location}/eda/pass_level_stats.parquet') pl
+                          LEFT JOIN read_parquet('{project_location}/eda/team_composition.parquet') dtc
+                            ON DEF_TEAM_COMPOSITION_PK = dtc.TEAM_COMPOSITION_PK 
+                          LEFT JOIN read_parquet('{project_location}/eda/team_composition.parquet') otc
+                            ON DEF_TEAM_COMPOSITION_PK = otc.TEAM_COMPOSITION_PK 
+                          )
+                      WHERE EVENT_ZONE_END LIKE 'O%'
+                      AND MIDFIELDERS_ATTACKING_GROUP_ID IS NOT NULL
+                      AND MIDFIELDERS_DEFENDING_GROUP_ID IS NOT NULL
+                      GROUP BY MIDFIELDERS_ATTACKING_GROUP_ID, FORWARDS_ATTACKING_GROUP_ID,
+                      --player_id, 
+                      EVENT_ZONE_END, team_id,
+                      --BACKS_DEFENDING_GROUP_ID, MIDFIELDERS_DEFENDING_GROUP_ID, 
+                      --FORWARDS_ATTACKING_GROUP_ID, MIDFIELDERS_ATTACKING_GROUP_ID, 
+                      MIDFIELDERS_DEFENDING,  BACKS_DEFENDING, MIDFIELDERS_ATTACKING, FORWARDS_ATTACKING
+                      ORDER BY pass_goal_assist DESC
+                     """)
+print(y_coords)
 
 
 # y_coords = duckdb.sql(f"""
@@ -211,64 +238,3 @@ project_location = 'C:/Users/Tyler/Documents/GitHub/soccer-analytics-capstone-te
 #                      """)
 # print(y_coords)
 
-
-y_coords = duckdb.sql(f"""
-                      with score_info as (
-                       SELECT match_id, period, start_date, end_date, home_goals, away_goals
-                       FROM read_parquet('{project_location}/eda/match_score_timeline.parquet')
-                       ),
-                       track_times as (
-                       SELECT match_id, period, interval_start
-                       FROM read_parquet('{project_location}/eda/period_lineups.parquet')
-
-                       UNION
-
-                       SELECT match_id, period, start_date
-                       FROM score_info
-                       ),
-                       id_changes as (
-                       SELECT track_times.*, home_goals, away_goals, player_id, country_id, POSITION_SIDE_ADJ, POSITION_TYPE, POSITION_TYPE_ALT, POSITION_BEHAVIOR, PLAYERS_SAME_COUNTRY, 
-                       PLAYERS_DIFF_COUNTRY, POSITION_SAME_COUNTRY, POSITION_DIFF_COUNTRY, SIDE_SAME_COUNTRY, SIDE_DIFF_COUNTRY,
-
-                       CASE
-                       WHEN IFNULL(LAG(track_times.period,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != track_times.period THEN 1
-                       WHEN IFNULL(LAG(home_goals,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != home_goals THEN 1
-                       WHEN IFNULL(LAG(away_goals,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != away_goals THEN 1
-                       WHEN IFNULL(LAG(POSITION_SIDE_ADJ,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),'N/A') != POSITION_SIDE_ADJ THEN 1
-                       WHEN IFNULL(LAG(POSITION_TYPE,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),'N/A') != POSITION_TYPE THEN 1
-                       WHEN IFNULL(LAG(POSITION_TYPE_ALT,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),'N/A') != POSITION_TYPE_ALT THEN 1
-                       WHEN IFNULL(LAG(POSITION_BEHAVIOR,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),'N/A') != POSITION_BEHAVIOR THEN 1
-                       WHEN IFNULL(LAG(PLAYERS_SAME_COUNTRY,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != PLAYERS_SAME_COUNTRY THEN 1
-                       WHEN IFNULL(LAG(PLAYERS_DIFF_COUNTRY,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != PLAYERS_DIFF_COUNTRY THEN 1
-                       WHEN IFNULL(LAG(POSITION_SAME_COUNTRY,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != POSITION_SAME_COUNTRY THEN 1
-                       WHEN IFNULL(LAG(POSITION_DIFF_COUNTRY,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != POSITION_DIFF_COUNTRY THEN 1
-                       WHEN IFNULL(LAG(SIDE_SAME_COUNTRY,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != SIDE_SAME_COUNTRY THEN 1
-                       WHEN IFNULL(LAG(SIDE_DIFF_COUNTRY,1) OVER (PARTITION BY track_times.match_id, track_times.period, player_id ORDER BY track_times.match_id, track_times.period, player_id, track_times.interval_start),-1) != SIDE_DIFF_COUNTRY THEN 1
-                       ELSE 0
-                       END AS iso_row
-                       FROM track_times
-                       LEFT JOIN score_info
-                        ON track_times.match_id = score_info.match_id
-                        AND track_times.period = score_info.period
-                        AND track_times.interval_start >= score_info.start_date
-                        AND track_times.interval_start < IFNULL(score_info.end_date, TODAY())
-                        LEFT JOIN read_parquet('{project_location}/eda/period_lineups.parquet') pl
-                        ON track_times.match_id = pl.match_id
-                        AND track_times.period = pl.period
-                        AND track_times.interval_start >= pl.interval_start
-                        AND track_times.interval_start < IFNULL(pl.interval_end, TODAY())
-                        )
-
-                        SELECT iso_changes.*, LEAD(interval_start) OVER (PARTITION BY match_id, period, player_id ORDER BY match_id, period, player_id, interval_start) interval_end
-                        FROM (
-                        SELECT match_id, period, home_goals, away_goals, player_id, country_id, POSITION_SIDE_ADJ, POSITION_TYPE, POSITION_TYPE_ALT, POSITION_BEHAVIOR, PLAYERS_SAME_COUNTRY, 
-                       PLAYERS_DIFF_COUNTRY, POSITION_SAME_COUNTRY, POSITION_DIFF_COUNTRY, SIDE_SAME_COUNTRY, SIDE_DIFF_COUNTRY, interval_start
-                       FROM id_changes
-                       WHERE iso_row = 1
-                       ) iso_changes
-
-                       ORDER BY match_id, period, player_id, interval_start
-
-
-                     """)
-print(y_coords)
